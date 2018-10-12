9acf1ef5aeee0009164ced6a3189a11a2b82786e
Александр Колосовский <663503@vipceiling.ru>
2018-10-12 12:41:50 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .wrapper
+    #ap
+      h1 Hello
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus">
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,5 @@
+import Router from 'vue-router'
+
+export default new Router({
+  routes: []
+})
