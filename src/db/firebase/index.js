426a084cd348858cbeed66c90427556e6194eb9c
Александр Колосовский <663503@vipceiling.ru>
2018-10-17 11:45:37 +0500

    edit path config

--- src/db/firebase/index.js
+++ src/db/firebase/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import {config} from './config/index'
+import {config} from './config/'
 
 const firebaseApp = firebase.initializeApp(config)
 const db = firebaseApp.database()
