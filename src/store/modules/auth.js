48c54714326dc0036f1fd1ee6a0851f34183d7bc
Alexandr Kolosovsky <kolo-ky@yandex.ru>
2018-10-18 22:49:29 +0500

    update auth module

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,7 @@
+import {AUTH_REQUEST, AUTH_SUCCESS, AUTH_ERROR, AUTH_LOGOUT} from '../actions/auth'
+import {USER_REQUEST} from '../actions/user'
+import {signIn} from '@/db/firebase/auth'
+
 export default {
   namespaced: true,
   state: {
@@ -9,6 +13,47 @@ export default {
     isAuthenticated: state => !!state.token,
     authStatus: state => state.status
   },
-  actions: { },
-  mutations: { }
+  actions: {
+    [AUTH_REQUEST]: ({commit, dispatch}, user) => {
+      return new Promise((resolve, reject) => {
+        commit(AUTH_REQUEST)
+        signIn(user.name, user.password)
+          .then(response => {
+            localStorage.setItem('user-token', response.token)
+            commit(AUTH_SUCCESS, response)
+            dispatch(USER_REQUEST)
+            resolve(response)
+          })
+          .catch(error => {
+            commit(AUTH_ERROR, error)
+            localStorage.removeItem('user-token')
+            reject(error)
+          })
+      })
+    },
+    [AUTH_LOGOUT]: ({commit, dispatch}) => {
+      return new Promise((resolve, reject) => {
+        commit(AUTH_LOGOUT)
+        localStorage.removeItem('user-token')
+        resolve()
+      })
+    }
+  },
+  mutations: {
+    [AUTH_REQUEST]: (state) => {
+      state.status = 'loading'
+    },
+    [AUTH_SUCCESS]: (state, resp) => {
+      state.status = 'success'
+      state.token = resp.token
+      state.hasLoadedOnce = true
+    },
+    [AUTH_ERROR]: (state) => {
+      state.status = 'error'
+      state.hasLoadedOnce = true
+    },
+    [AUTH_LOGOUT]: (state) => {
+      state.token = ''
+    }
+  }
 }
